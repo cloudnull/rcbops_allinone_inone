f1f475f4c67d9130285a8517adb75758498230a0
cloudnull <kevin.carter@rackspace.com>
2013-11-07 23:54:26 -0600

    added no error checking if epel is already installed.

--- rcbops_allinone_inone.sh
+++ rcbops_allinone_inone.sh
@@ -225,9 +225,11 @@ function install_yum_packages() {
 
   # Install ERLANG
   pushd /tmp
+  set +e
   wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
   wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
   rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
+  set -e
   popd
   yum -y install erlang
 
